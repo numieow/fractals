d1b702e4945e2406a49cf3753760bf4228ac0751
numieow <maximus.wirth@outlook.fr>
2024-08-20 13:53:51 +0200

    [+] Ranges corrections

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,18 +3,19 @@ from tqdm import tqdm
 import os
 import colorsys
 import math
+import time
 
 # Generates Mandelbrot set
 # Iterates over every coordinates 
 # Stop condition is ||z_n|| > 2
 
-WIDTH = 1000
-HEIGHT = round(WIDTH * 16/9)
-X_RANGE = 4
-Y_RANGE = X_RANGE * 16/9
-MIN_X, MAX_X = (-X_RANGE/2, X_RANGE/2)
-MIN_Y, MAX_Y = (-Y_RANGE/2, Y_RANGE/2)
-MAX_STEPS = 250
+HEIGHT, WIDTH = 1500, 1500
+MIN_X, MAX_X = -1.5, 0.7
+MIN_Y, MAX_Y = -1.25, 1.25
+X_RANGE, Y_RANGE = MAX_X - MIN_X, MAX_Y - MIN_Y
+MAX_STEPS = 500
+
+# TODO : Use complex type 
 
 rendu = Image.new('RGB', (WIDTH, HEIGHT), color='black')
 pixels = rendu.load()
@@ -23,6 +24,8 @@ def color_scale(distance, exp, const):
     color = distance ** exp
     rgb_color = colorsys.hsv_to_rgb(const + color, 1 - color, 1)
     return tuple(round(i*255) for i in rgb_color)
+
+start = time.time()
 
 for row in tqdm(range(HEIGHT)):
     for col in range(WIDTH):
@@ -42,6 +45,10 @@ for row in tqdm(range(HEIGHT)):
         if i < MAX_STEPS:
             distance = (i + 1) / (MAX_STEPS + 1)
             pixels[col, row] = color_scale(distance, 0.2, 0.3)
+
+end = time.time()
+
+print("Execution time : {:.2f}".format(end - start))
 
 rendu.save('images/mandelbrot.png')
 os.system('open images/mandelbrot.png')
